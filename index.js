f1a59b610bd5d20f0690f3b7554e85910ea0a9d0
mHamza301 <mHamza301@users.noreply.github.com>
2020-10-06 15:11:48 +0500

    The frontend is not working properly. Due to error I am facing inContent Security Policy for accessing the CDN of VUE.JS. Backend and API hits are working fine.

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
 
+require('dotenv').config();
+
+//Database Connection
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+urls.createIndex({alias: 1}, {unique: true});
 
 const app = express();
 
@@ -15,25 +24,83 @@ app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.static('./public'));
 
-/*
+//Routes
 
-app.get('/url/:id', (req, res) =>{
+app.get('/:id', async (req, res, next) =>{
 
-
-});
-
-
-app.get('/:id', (req, res) =>{
-
+    const {id: alias} = req.params;
+    try {
+        const url = await urls.findOne({alias});
+        if (url) {
+            res.redirect(url.url);
+        }
+        res.redirect(`/?error=${alias} not found`);
+    } catch (error) {
+        res.redirect(`/?error= Link not found`);
+    }
 
 });
  
+//Schema Validation
 
-app.post('/url', (req, res) =>{
+const schema = yup.object().shape({
 
+    alias: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
 
 });
-*/
+app.post('/url', async (req, res, next) =>{
+
+    let {alias, url} = req.body;
+
+    try {
+        await schema.validate({
+            alias,
+            url,
+        });
+
+        if (!alias) {
+            alias = nanoid(12);
+        }  else {
+            const alreadyPresent = await urls.findOne({alias});
+                if (alreadyPresent){
+                    throw new Error('Alias already present');
+                }
+        } 
+        alias = alias.toLowerCase();
+
+        const newURL = {
+            url,
+            alias,
+        };
+
+        const createdUrl = await urls.insert(newURL);
+        res.json(createdUrl);
+    } catch (error)
+    {
+        next(error);
+    }
+
+});
+
+//Error Handling
+
+app.use((error, req, res, next) =>{
+
+    if(error.status){
+        res.status(error.status);
+    }
+    else{
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack : process.env.NODE_ENV === 'production' ? 'Good' : error.stack,
+        
+    })
+});
+
+//Localhost running setup
 
 const port = process.env.PORT || 5555;
 app.listen(port, () =>{
---
+++ public/app.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      url: '',
+      alias: '',
+      error: '',
+      formVisible: true,
+      created: null,
+    },
+    methods: {
+      async createUrl() {
+        this.error = '';
+        const response = await fetch('/url', {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            url: this.url,
+            alias: this.alias || undefined,
+          }),
+        });
+        if (response.ok) {
+          const result = await response.json();
+          this.formVisible = false;
+          this.created = `http://muhammad.hamza/${result.alias}`;
+        } else if (response.status === 429) {
+          this.error = 'You are sending too many requests. Try again in 30 seconds.';
+        } else {
+          const result = await response.json();
+          this.error = result.message;
+        }
+      },
+    },
+  });
